166a6f6921093b58a9358061e6d95c5fd9513d96
ankit <ankit0905anand@gmail.com>
2017-04-14 16:19:55 +0530

    added appropriate files

--- __init__.py
+++ __init__.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, flash, request, url_for, redirect, session
 
 app = Flask(__name__)
+app.secret_key = 'my_random_key'
 
 @app.route('/')
 def home():
-	return "Welcome to homepage"
+	return render_template("header.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html')
+	
+@app.errorhandler(405)
+def method_not_found(e):
+	return render_template('405.html')
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(debug=True, port=8000)
 
